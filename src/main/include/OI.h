d43d89ea5d6e65b6489fa0bbf633145991680551
smutnyj <smutnyj@mail.gvsu.edu>
2019-01-06 09:58:01 -0500

    Defined Controller buttons/Motors. Mapped constant values to buttons.

--- src/main/include/OI.h
+++ src/main/include/OI.h
@@ -7,10 +7,27 @@
 
 #pragma once
 
-#include <frc/Joystick.h>     //Driving w/ Joystick
+#include <Joystick.h>     //Driving w/ Joystick
+#include <JoystickButton.h>
 
 class OI {
  public:
   OI();
   frc::Joystick driveStick{0};
+
+  private:
+  // see "Running commands on Joystick input" on WPIlib
+	frc::JoystickButton* ClawOpen;
+	frc::JoystickButton* ClawClose;
+
+  //  Button functions for controller
+  frc::JoystickButton* HatchOpen;   //Set Claw position to prepare to pick up Hatch
+  frc::JoystickButton* HatchClose;  //Set Claw position to secure hatch
+  frc::JoystickButton* CargoOpen;   //Set Claw position to prepare to pick up Cage
+  frc::JoystickButton* CargoClose;  //Set Claw position to secure Cargo
+  frc::JoystickButton* ClawRelease; //Trigger Solinoid to release vertical claw at start of match
+  frc::JoystickButton* Align;       //Initiallize Autonomous system to align robot to ship/rocket
+  frc::JoystickButton* Height1;     //Set Claw Height to Lv1 (1'7")
+  frc::JoystickButton* Height2;	    //Set Claw Height to Lv2 (3'11")
+  frc::JoystickButton* Height3;	    //Set Claw Height to Lv3 (6'3")
 };
