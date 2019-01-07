a0c0ae4034cb9a19db3e1acc0fe16f3a4c643e7d
Dcbaas <bcdave.301@gmail.com>
2019-01-07 17:03:04 -0500

    Finished Vision header, moved pipline to libs

---
+++ src/main/cpp/subsystems/VisionSystem.cpp
@@ -0,0 +1,6 @@
+#include "subsystem/VisionSystem.h"
+
+namespace{
+    
+}
+
---
+++ src/main/include/subsystems/VisionSystem.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <frc/commands/Subsystem.h>
+#include "cscore.h"
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>\
+
+#include "libraries/GroundTapePipline.h"
+
+namespace Camera{
+    class AligmentCamera : public frc::Subsystem{
+    public:
+        void InitDefaultCommand() override;
+        int findDistance();
+    private:
+        cs::UsbCamera camera;
+        cs::CvSink cameraSink;
+        cs::CvSource outStream;
+        cv::Mat sourceFrame;
+        grip::GroundTapePipline processPipline;
+    };
+}
