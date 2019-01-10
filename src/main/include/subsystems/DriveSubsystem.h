0c13019e2dc42cacc4a81c7809d5cbcb6540b85b
Kyle Flynn <kyle.flynn5798@yahoo.com>
2019-01-10 16:35:23 -0500

    Removed old SparkMax class. Changed to CAN.

--- src/main/include/subsystems/DriveSubsystem.h
+++ src/main/include/subsystems/DriveSubsystem.h
@@ -1,7 +1,6 @@
 #pragma once
 
-#include "libraries/SparkMax.h"
-#include <rev/SparkMax.h>
+#include <rev/CANSparkMax.h>
 #include "commands/DriveCommand.h"
 #include <frc/commands/Subsystem.h>
 #include <frc/drive/DifferentialDrive.h>
@@ -15,14 +14,10 @@ class DriveSubsystem : public frc::Subsystem {
   void Stop();
 
  private:
-  // SparkMax frontLeft{6};
-  // SparkMax backLeft{5};
-  // SparkMax frontRight{3};
-  // SparkMax backRight{4};
-  rev::SparkMax frontLeft{6};
-  rev::SparkMax backLeft{5};
-  rev::SparkMax frontRight{3};
-  rev::SparkMax backRight{4};
+  rev::CANSparkMax frontLeft{6, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax backLeft{5, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax frontRight{3, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax backRight{4, rev::CANSparkMax::MotorType::kBrushless};
   frc::SpeedControllerGroup left{frontLeft, backLeft};
   frc::SpeedControllerGroup right{frontRight, backRight};
   frc::DifferentialDrive robotDrive{left, right};
